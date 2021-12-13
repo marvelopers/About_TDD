5580cfeb25824c770e21cec8041f8dc5cf2d1161
marvelopers <marvelopers@gmail.com>
2021-12-13 23:19:07 +0900

    에러 tdd

---
+++ unit-test/src/async.js
@@ -0,0 +1,9 @@
+const fetchCoin = (error) => {
+  if(error === 'error'){
+    return Promise.reject('network error')
+  }
+
+  return Promise.resolve({item: 'bitcoin', price: 60000000})
+}
+
+module.exports = fetchCoin;
