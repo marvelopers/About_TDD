59bc82307108c16208811021e4ca11ba788f35be
marvelopers <marvelopers@gmail.com>
2022-07-05 22:01:56 +0900

    Mock을 사용하는 테스트

---
+++ unit-test/src/mock/product/test/product_service_no_di.test.js
@@ -0,0 +1,33 @@
+const ProductService = require('../product_service_no_di.js');
+const ProductClient = require('../product_client.js');
+jest.mock('../product_client');
+
+describe('ProductService', () => {
+  const fetchItems = jest.fn(async () => [
+    { item: '🥛', available: true },
+    { item: '🍌', available: false },
+  ]);
+  ProductClient.mockImplementation(() => {
+    return {
+      fetchItems,
+    };
+  });
+  let productService;
+
+  beforeEach(() => {
+    productService = new ProductService();
+    fetchItems.mockClear();
+    ProductClient.mockClear();
+  });
+
+  it('should filter out only available items', async () => {
+    const items = await productService.fetchAvailableItems();
+    expect(items.length).toBe(1);
+    expect(items).toEqual([{ item: '🥛', available: true }]);
+  });
+
+  it('test', async () => {
+    const items = await productService.fetchAvailableItems();
+    expect(fetchItems).toHaveBeenCalledTimes(1);
+  });
+});
